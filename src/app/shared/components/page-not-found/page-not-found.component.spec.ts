e1d8c3e13b26734428ea599df92110eafdbfe319
amirkhabbax <amir.khabbaz@gmail.com>
2025-05-06 19:29:02 +0330

    page not found works now! with unit tests

--- src/app/shared/components/page-not-found/page-not-found.component.spec.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageNotFoundComponent } from './page-not-found.component';
+import { LanguageHandlerService } from '../../services/language-handler.service';
 
 describe('PageNotFoundComponent', () => {
   let component: PageNotFoundComponent;
@@ -8,9 +9,8 @@ describe('PageNotFoundComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageNotFoundComponent]
-    })
-    .compileComponents();
+      imports: [PageNotFoundComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PageNotFoundComponent);
     component = fixture.componentInstance;
@@ -19,5 +19,19 @@ describe('PageNotFoundComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should get the loader' visibility from the service", () => {
+    const fixture = TestBed.createComponent(PageNotFoundComponent);
+    const app = fixture.componentInstance;
+    let languageHandlerService = fixture.debugElement.injector.get(
+      LanguageHandlerService
+    );
+    fixture.detectChanges();
+    let isPersian = languageHandlerService.langauge() == 'fa';
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toEqual(
+      isPersian ? '404 صفحه مورد نظر یافت نشد!' : '404 Page Not Found!'
+    );
   });
 });
--- src/app/shared/components/page-not-found/page-not-found.component.ts
+++ src/app/shared/components/page-not-found/page-not-found.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LanguageHandlerService } from '../../services/language-handler.service';
 
 @Component({
   selector: 'app-page-not-found',
   imports: [],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  styleUrl: './page-not-found.component.scss',
 })
 export class PageNotFoundComponent {
+  private languageHandlerService = inject(LanguageHandlerService);
 
+  get isPersian() {
+    return this.languageHandlerService.langauge() == 'fa';
+  }
 }
