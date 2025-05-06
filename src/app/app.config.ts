5f689d8c467fafd61b64ba13dffb570f6f77aef9
amirkhabbax <amir.khabbaz@gmail.com>
2025-05-06 13:11:18 +0330

    global Error handler added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, TitleStrategy } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppNameTitleStrategyService } from './shared/services/app-name-title-strategy.service';
 import { HttpLoaderInterceptor } from './shared/functions/HttpLoaderInterceptor';
+import { GlobalErrorHandlerComponent } from './shared/components/global-error-handler/global-error-handler.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptors([HttpLoaderInterceptor])),
     { provide: TitleStrategy, useClass: AppNameTitleStrategyService },
+    {provide: ErrorHandler , useClass: GlobalErrorHandlerComponent}
   ],
 };
---
+++ src/app/shared/components/global-error-handler/global-error-handler.component.ts
@@ -0,0 +1,39 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, ErrorHandler, inject } from '@angular/core';
+import Swal from 'sweetalert2';
+import { LanguageHandlerService } from '../../services/language-handler.service';
+import { ThemesService } from '../../services/themes.service';
+@Component({
+  selector: 'app-global-error-handler',
+  imports: [],
+  templateUrl: './global-error-handler.component.html',
+  styleUrl: './global-error-handler.component.scss',
+})
+export class GlobalErrorHandlerComponent extends ErrorHandler {
+  private languageHandlerService = inject(LanguageHandlerService);
+  private themesService = inject(ThemesService);
+  override handleError(error: any): void {
+    if (!(error instanceof HttpErrorResponse)) {
+      error = error.rejction;
+    }
+
+    let errorMessage: string =
+      error?.message ??
+      (this.languageHandlerService.langauge() == 'fa'
+        ? 'خطای ناشناخته'
+        : 'Undefined error!');
+    let errorFooterMessage: string = `${error?.status} ${error?.statusText}`;
+
+    Swal.fire({
+      icon: 'error',
+      title:
+        this.languageHandlerService.langauge() == 'fa' ? 'خطا...' : 'Oops...',
+      theme: this.themesService.isDarkMode() ? 'dark' : 'light',
+      text: errorMessage,
+      footer:
+        errorFooterMessage ?? (this.languageHandlerService.langauge() == 'fa'
+          ? 'وضعیت خطای ناشناخته'
+          : 'Undefinded error status'),
+    });
+  }
+}
